19ca8c822a2e5591fd3cb3bee7e4b632e0ded2a1
Rishi Chaubey <155433512+RishiChaubey31@users.noreply.github.com>
2025-07-06 13:36:15 +0530

    setup: integrated ShadCn ui (#53)

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,9 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return <div>Hello World!</div>;
+  return <div className="flex min-h-screen flex-col items-center py-8 ">
+    <h1>Hello World!</h1>
+    <Button >Click Me</Button>
+
+  </div>;
 }

9be4606834610ac2a0a01bc4458e57d5cd64de63
Anuj Chhikara <107175639+AnujChhikara@users.noreply.github.com>
2025-08-08 18:59:56 +0530

    fix(todo-list-table): update include done tasks toggle logic  (#175)
    * fix(todo-list-table): update include done tasks toggle logic for better visibility
    
    * fix(todo-list-table): ensure include done tasks toggle correctly reflects boolean value
    
    ---------
    
    Co-authored-by: anuj.k <anuj.k@codonsystems.com>

--- components/todo-list-table.tsx
+++ components/todo-list-table.tsx
@@ -180,6 +180,7 @@ export const TodoListTable = ({
 
   const search = searchParams.get(QUERY_PARAMS_KEYS.search) ?? ''
   const currentTab = searchParams.get('tab')
+  const showIncludeDoneTasksToggle = currentTab === DashboardTasksTableTabs.All || !currentTab
   const showDeferredColumn =
     currentTab === DashboardTasksTableTabs.Deferred ||
     currentTab === DashboardTasksTableTabs.WatchList
@@ -216,11 +217,11 @@ export const TodoListTable = ({
           containerClassName="w-full lg:max-w-xs"
           onChange={(e) => handleSearch(e.target.value)}
         />
-        {currentTab == DashboardTasksTableTabs.All && (
+        {showIncludeDoneTasksToggle && (
           <div className="flex px-4">
             <Switch
               id="includeDoneTasks"
-              checked={includeDone}
+              checked={!!includeDone}
               onCheckedChange={(checked) => onIncludeDoneChange?.(!!checked)}
             />
             <Label htmlFor="includeDoneTasks" className="px-2">

7d86b7e604e37145bcf7f721a00ee0f697121187
Hariom Vashista <rishivashista4@gmail.com>
2025-09-17 03:08:35 +0530

    fix(#207): add class to description textarea to prevent layout breaking (#209)
    * fix(creat-edit-todo-form): add class to description textarea to prevent layout breaking
    
    * fix(create-todo-form): add max height to description textarea component
    
    * fix(create-edit-todo-form): wrap form in a div with the min width set to
    0 to fix layout issues
    
    * fix(create-todo-form): add custom utility to use word-break in textarea
    
    * fix(create-todo-form): remove unecessary div

--- src/components/todos/create-edit-todo-form.tsx
+++ src/components/todos/create-edit-todo-form.tsx
@@ -179,6 +179,7 @@ export const CreateEditTodoForm = ({
         <Textarea
           id="description"
           placeholder="e.g Nothing is cool in here"
+          className="break-word max-h-32"
           {...register('description')}
         />
       </FormInput>
